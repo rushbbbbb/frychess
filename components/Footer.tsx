b26096a7c8ec6c326913ac851d2816397a672497
rushbbbbb <1531407383@qq.com>
2024-08-13 23:09:32 +0800

    change some
    cs

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer(props: JSX.HTMLAttributes<HTMLButtonElement>) {
 			<div class="text-center mt-8 p-2">
 				<a class="bg-red-700 border rounded py-2 px-4 font-black" href="/about">关于</a>
 			</div>
-			<div class="text-center mt-8 text-pink-100  text-sm">@17fei.fun</div>
+			
 		</>
 	);
 }
--- routes/_app.tsx
+++ routes/_app.tsx
@@ -6,8 +6,7 @@ export default function App({ Component }: AppProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="情侣情趣飞行棋是一款包含不同尺度适合不同阶段情侣一起玩的情趣游戏" />
-        <title>情侣飞行棋 - 情趣私密的情侣小游戏</title>
+        <title>情侣小游戏</title>
         <script src="/stat.js"></script>
       </head>
       <body>
